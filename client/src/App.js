ae12ea9b3fceb06ae2947e25d401043e2af0853c
n.hisso <n.hisso@haykalmedia.com>
2022-03-01 14:26:19 +0400

    JWT Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import { getPosts } from "./actions/posts";
 import Navbar from "./components/Navbar/Navbar";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-
-import useStyles from "./styles";
+import Home from "./components/Home/Home";
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxWidth="md">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-            className={classes.mainContainer}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth="md">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
